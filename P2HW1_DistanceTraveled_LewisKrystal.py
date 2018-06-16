9d0c14f334a652702111b54e1a335736da91c665
lewisk5533 <40321139+lewisk5533@users.noreply.github.com>
2018-06-16 11:51:21 -0400

    Add files via upload
    Second Submission

---
+++ P2HW1_DistanceTraveled_LewisKrystal.py
@@ -0,0 +1,38 @@
+#CTI-110
+#P2HW1 - Distance Traveled
+#Krystal Lewis
+#6/16/2018
+
+#This program calculates the distance a car will travel per hour
+speed = int(input("speed: ")) 
+time1 = int(input("distanceAfter6: "))
+distance1 = speed * time1
+print("The distance the car will travel is",distance1,"hours.")
+
+time2 = int(input("distanceAfter10: "))
+distance2 = speed * time2
+print("The distance the car will travel is",distance2,"hours.")
+
+time3 = int(input("distanceAfter15: "))
+distance3 = speed * time3
+print("The distance the car will travel is",distance3,"hours.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
