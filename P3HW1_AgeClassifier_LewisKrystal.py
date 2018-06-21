fbc0b958ee628421fabc50dac5ddcb46cae07d51
lewisk5533 <40321139+lewisk5533@users.noreply.github.com>
2018-06-21 13:37:21 -0400

    Add files via upload
    Age classifier

---
+++ P3HW1_AgeClassifier_LewisKrystal.py
@@ -0,0 +1,22 @@
+#Use the if/else structure to determine a person's age
+
+#Get input from user
+
+
+age = int(input("The age of the person is: "))
+
+if age <= 1:
+    print("He/She is an infant.")
+
+elif age <= 13:
+    print("He/She is a child.")
+
+elif age <= 20:
+    print ("He/She is a teenager.")
+
+else:
+    print("He/she is an adult.")
+    
+    
+    
+
