9726ae1a3620ba66773acf4573271c8b65621ad4
lewisk5533 <40321139+lewisk5533@users.noreply.github.com>
2018-07-20 07:50:13 -0400

    Add files via upload
    Snowflake

---
+++ P4LAB_Lewis.py
@@ -0,0 +1,18 @@
+#Snowflake program
+#Krystal Lewis
+#6/30/2018
+
+import turtle
+import random
+
+wn = turtle.Screen()
+
+for i in range(10):
+    for i in range(2):
+        turtle.forward(100)
+        turtle.right(60)
+        turtle.forward(100)
+        turtle.right(120)
+    turtle.right(36)
+    
+wn.exitonclick()
